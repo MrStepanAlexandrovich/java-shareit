5c8a9d10adb9befbe1a729b9f2b257bc66cd532b
mr.Stepan <golenduhinsa@gmail.com>
2026-01-13 21:44:20 +0500

    -создан модуль gateway

---
+++ gateway/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.request;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemRequestController {
+}
---
+++ gateway/src/main/java/ru/practicum/shareit/user/dto/UserEditDto.java
@@ -0,0 +1,16 @@
+package ru.practicum.shareit.user.dto;
+
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UserEditDto {
+    private int id;
+    private String name;
+
+    @Email
+    private String email;
+
+}
