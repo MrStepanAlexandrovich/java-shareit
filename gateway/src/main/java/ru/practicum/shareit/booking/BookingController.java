ef30241b00f24b4dffb0c7eb01e3e37d20cb3c42
MrStepanAlexandrovich <stepangolenduhin@yandex.ru>
2026-01-15 13:08:29 +0500

    -

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -37,7 +37,7 @@ public class BookingController {
 	@PostMapping
 	public ResponseEntity<Object> bookItem(
 			@RequestHeader("X-Sharer-User-Id") @Positive long userId,
-			@RequestBody @Valid BookItemRequestDto requestDto
+			@RequestBody BookItemRequestDto requestDto
 	) {
 		log.info("Creating booking {}, userId={}", requestDto, userId);
 		return bookingClient.bookItem(userId, requestDto);
--- gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -13,7 +13,6 @@ import java.time.LocalDateTime;
 public class CommentDto {
     private int id;
 
-    @NotBlank
     private String authorName;
 
     private LocalDateTime created;
